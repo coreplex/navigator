20ee4e64aae1b817848a6b377158a2ab58b37690
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2015-04-07 12:24:07 +0100

    Initial Commit

---
+++ src/NavigatorServiceProvider.php
@@ -0,0 +1,12 @@
+<?php namespace Coreplex\Navigator;
+
+use Illuminate\Support\ServiceProvider;
+
+class NavigatorServiceProvider extends ServiceProvider {
+
+    public function register()
+    {
+
+    }
+
+}

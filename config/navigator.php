c6e54b5175c6a57ac293c4496e44a9b417d88b0a
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2015-04-08 14:50:37 +0100

    Updated the package config

--- config/navigator.php
+++ config/navigator.php
@@ -2,53 +2,62 @@
 
 return [
 
+    /**
+     * =======================================================================
+     *  Base menu items
+     * =======================================================================
+     *
+     * You can set base menu items here if you are using the array store.
+     */
     'menus' => [
-        'main' => [
-            'items' => [
-                [
-                    'title' => 'Register',
-                    'items' => [
-                        [
-                            'title' => 'Test',
-                            'filter' => [
-                                'notAuthenticated' => [
-                                    ['test', 'test']
-                                ],
-                            ],
-                        ]
-                    ]
-                ],
-            ],
-            'view' => 'main'
-        ],
-
-        'sidebar' => [
-            'items' => [
-                [
-                    'title' => 'Dashboard',
-                    'url' => '/',
-                    'icon' => 'icon icon-meter',
-                    'filter' => 'notAuthenticated',
-                    'items' => [
-                        [
-                            'title' => 'Test',
-                            'url' => '/users',
-                        ]
-                    ]
-                ],
-            ],
-            'view' => 'sidebar'
-        ]
+        //
     ],
 
+    /**
+     * =======================================================================
+     *  Default view
+     * =======================================================================
+     *
+     * If you are using a templatable driver then you can set the base
+     * template here. To override this at run time you can use the template
+     * method.
+     */
     'defaultView' => 'navigator::bootstrap.default',
 
+    /**
+     * =======================================================================
+     *  Renderer
+     * =======================================================================
+     *
+     * Set the navigator renderer. Can be:
+     *
+     * Coreplex\Navigator\Renderers\LaravelBlade
+     */
     'renderer' => 'Coreplex\Navigator\Renderers\LaravelBlade',
 
+    /**
+     * =======================================================================
+     *  Store
+     * =======================================================================
+     *
+     * Set the data store you are using. Can be:
+     *
+     * Coreplex\Navigator\Store\ArrayStore
+     */
     'store' => 'Coreplex\Navigator\Store\ArrayStore',
 
+    /**
+     * =======================================================================
+     *  Item Filters
+     * =======================================================================
+     *
+     * Item filters can be used to verify if a user can view a menu item. To
+     * add a filter add them to the array below with the key as a unique name
+     * used to access the item filter and the value being then name of the
+     * class used for the filter.
+     */
     'filters' => [
-        'notAuthenticated' => 'AdvancedRent\Filters\NotAuthenticated',
+        //
     ],
 
 ];
